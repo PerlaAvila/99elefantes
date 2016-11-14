a27e230e759c6ba093c2950ada4b5ff5dcd8a870
codea_mac_04 <codea_mac_04@gmail.com>
2016-11-14 10:58:17 -0600

    elefantes

---
+++ elefantes.rb
@@ -0,0 +1,10 @@
+def elephants
+  counter = 0
+  while counter < 99
+    counter += 1
+     puts "#{counter} elefante se columpiaba sobre la tela de una araña como veía que resistía fueron a llamar a otro elefante."
+ end
+ print "100 elefantes rompieron la telaraña"
+end
+
+elephants
