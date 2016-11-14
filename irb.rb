f9d557324f378f56078d1dcef0dd690d01a83c32
codea_mac_04 <codea_mac_04@gmail.com>
2016-11-14 17:26:27 -0600

    hgsd

---
+++ irb.rb
@@ -0,0 +1,39 @@
+#suma
+5 + 6
+=> 11
+
+#suma string
+"pepe" + " come peras"
+"pepe come peras"
+
+#built-in method
+1.56787878.to_i
+=> 1
+
+#variable
+gato = "bonito"
+"bonito"
+"gatito " + gato
+=> "gatito bonito"
+
+#array
+array = ["gato", "perro", "ratón"]
+=> ["gato", "perro", "ratón"]
+
+#bang
+perro = "feo"
+ "feo"
+perro.capitalize!
+"Feo"
+
+#math
+Math.sqrt(7690939204)
+=> 87698.0
+
+#metodo
+def shout(string)
+  string.upcase
+  end
+=> :shout
+shout("hola")
+=> "HOLA"
