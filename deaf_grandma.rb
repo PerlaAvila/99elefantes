b80007b5dc8f815b994ac872a0579f30b89f0c16
codea_mac_04 <codea_mac_04@gmail.com>
2016-11-14 12:42:49 -0600

    grandma final

---
+++ deaf_grandma.rb
@@ -0,0 +1,23 @@
+def deaf_grandma
+     bye = 0
+    
+  while bye < 3
+    puts "¿Qué quieres contarle a la abuela?"
+    input = gets.chomp
+
+    if input == "BYE TQM"
+      bye += 1
+    end
+
+    if input != input.upcase
+      puts "NO, NO DESDE 1983"
+    else
+      puts "HUH?! NO TE ESCUCHO, HIJO!"
+    end
+  end
+  while bye = 3
+    puts "D'oh, adios mijito"
+    break
+  end
+end
+deaf_grandma
