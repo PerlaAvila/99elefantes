21e5dce3dfc2da5e442298ccba3ccdf1256bc893
phacohen <perla.avila.amaro@gmail.com>
2016-11-16 10:15:59 -0600

    Add files via upload

--- miercoles/linear.rb
+++ miercoles/linear.rb
@@ -1,17 +1,17 @@
-# def linear_search(num, array)
-#   i = 0
-#   while i < array.length
-#     if array[i] == num
-#       return i
-#   else
-#      nil
-#   end
-#   i +=1
-# end
-# end
-# random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
-# p linear_search(20, random_numbers)
-# p linear_search(29, random_numbers)
+def linear_search(num, array)
+  i = 0
+  while i < array.length
+    if array[i] == num
+      return i
+  else
+     nil
+  end
+  i +=1
+end
+end
+random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
+p linear_search(20, random_numbers)
+p linear_search(29, random_numbers)
 
 def global_linear_search(letter, array)
  i = 0
